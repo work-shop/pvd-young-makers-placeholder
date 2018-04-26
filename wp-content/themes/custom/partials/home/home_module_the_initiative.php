60f264801e0457fb669517775902ea2b16872523
Nic Schumann <nicolas_schumann@brown.edu>
2018-04-26 16:46:31 -0400

    [Mayor's Quote] added quotation from the Mayor to the text.

--- wp-content/themes/custom/partials/home/home_module_the_initiative.php
+++ wp-content/themes/custom/partials/home/home_module_the_initiative.php
@@ -1,12 +1,24 @@
 <section class="initiative-container container-fluid mb6">
     <div class="row">
-        <div class="offset-sm-1 offset-xs-0 col-xs-12 col-sm-7 home-module">
+        <div class="offset-xs-0 col-xs-12 offset-sm-1 col-sm-6 home-module">
 
             <div class="module-tag bg-brand-teal-dark">
                 <span class="h6 uppercase white bold">The Initiative</h5>
             </div>
 
-            <?php the_field('the_initiative_text', $id); ?>
+            <?php echo get_field('the_initiative_text', $id); ?>
+
+        </div>
+
+        <div class="offset-xs-0 col-xs-12 offset-sm-1 col-sm-3 home-module mayors-quotation">
+
+            <h5 class="quote-text bold mb2">
+
+                &ldquo;<?php echo get_field('quotation', $id); ?>&rdquo;
+
+            </h5>
+
+            <h6 class="mayors-name bold h6">– <?php echo get_field('mayors_name', $id); ?></h6>
 
         </div>
     </div>
