be3b858ff4f5c23bf63f1a76e9070bf1920c91d7
Nic Schumann <nicolas_schumann@brown.edu>
2018-05-01 15:02:40 -0400

    [Landing Layout] centered some text

--- wp-content/themes/custom/partials/home/home_hero.php
+++ wp-content/themes/custom/partials/home/home_hero.php
@@ -1,14 +1,14 @@
 <?php  ?>
 
-<section id="home-callout" class="container-fluid mb6">
+<section id="home-callout" class="container-fluid mb8">
     <div class="row ">
         <div class="col-xs-8 offset-xs-2 offset-md-1 col-md-3 mb4">
 
             <?php include( locate_template('partials/logo.php') ); ?>
 
         </div>
-        <div class="col-xs-10 col-md-5 col-lg-5 offset-xs-1">
-            <h1 class="bold brand-teal-dark centered-xs">
+        <div class="offset-xs-1 col-xs-10 col-md-5">
+            <h1 class="bold brand-teal-dark centered-xs home-callout-text">
                 <?php echo get_field( 'page_hero_text', $id ); ?>
             </h1>
         </div>
