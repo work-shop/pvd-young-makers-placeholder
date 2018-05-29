3d43f0911aa34e633cc3e94b0cfd08a3d0cf2b75
Nic Schumann <nicolas_schumann@brown.edu>
2018-05-29 10:34:38 -0400

    [Schedule] added a link to the schedule to the bottom of the site.

--- wp-content/themes/custom/partials/contact.php
+++ wp-content/themes/custom/partials/contact.php
@@ -4,7 +4,16 @@
             <h2 class="white">Full Website and Press Release Coming Soon</h2>
         </div>
     </div>
+</section>
 
+<section class="contact-button container-fluid schedule-button" data-modal-target="contact">
+    <div class="row">
+        <div class="col-xs-10 offset-xs-1 centered p2">
+            <a href="https://fabnewport.org/pvd-young-makers-weekly-schedule/" target="_blank" class="h2 contact-button-text schedule-button-text">
+                See our schedule!
+            </a>
+        </div>
+    </div>
 </section>
 
 <section class="contact-button container-fluid modal-toggle" data-modal-target="contact">
