7306b8e2fb72b9a3dac595d799500db8a3bc5d9c
Nic Schumann <nicolas_schumann@brown.edu>
2018-06-01 09:00:12 -0400

    [Press Release] added a link to the press release to the placeholder site.

--- wp-content/themes/custom/partials/contact.php
+++ wp-content/themes/custom/partials/contact.php
@@ -1,25 +1,31 @@
 <section id="contact-us" class="container-fluid">
     <div class="">
         <div class="col-xs-10 offset-xs-1 centered">
-            <h2 class="white">Full Website and Press Release Coming Soon</h2>
+            <h2 class="white">Full Website Coming Soon!</h2>
         </div>
     </div>
 </section>
 
 <section class="contact-button container-fluid schedule-button" data-modal-target="contact">
     <div class="row">
-        <div class="col-xs-10 offset-xs-1 centered p2">
-            <a href="https://fabnewport.org/pvd-young-makers-weekly-schedule/" target="_blank" class="h2 contact-button-text schedule-button-text">
-                See our schedule!
-            </a>
-        </div>
+        <a href="http://www.providenceri.gov/mayor-elorza-announces-pvd-young-makers-program/" target="_blank" class="col-xs-10 offset-xs-1 centered contact-button-text schedule-button-text">
+            <h2 class="contact-button-text p1">Read the Press Release</h2>
+        </a>
+    </div>
+</section>
+
+<section class="contact-button container-fluid schedule-button" data-modal-target="contact">
+    <div class="row">
+        <a href="https://fabnewport.org/pvd-young-makers-weekly-schedule/" target="_blank" class="h2 col-xs-10 offset-xs-1 centered contact-button-text schedule-button-text">
+            <h2 class="contact-button-text p1">See our schedule!</h2>
+        </a>
     </div>
 </section>
 
 <section class="contact-button container-fluid modal-toggle" data-modal-target="contact">
     <div class="row">
-        <div class="col-xs-10 offset-xs-1 centered p2">
-            <h2 class="contact-button-text">Contact us for more info!</h2>
+        <div class="col-xs-10 offset-xs-1 centered">
+            <h2 class="contact-button-text p1">Contact us for more info!</h2>
         </div>
     </div>
 </section>
